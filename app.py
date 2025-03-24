e64929541ea34f083447f9cfbe7e5e7e874f6466
solveonyx <jshulfer@solveonyx.com>
2025-03-24 18:44:50 -0500

    Fix: properly bind to dynamic Render port

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def get_customers():
 # Run the app
 if __name__ == '__main__':
     import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
